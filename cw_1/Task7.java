ceda32487b6b21880c109f8bd17fc145086a91be
ShcharbitskiAV <scarbicki@gmail.com>
2022-07-16 15:42:02 +0300

    new files

---
+++ cw_1/Task7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner word = new Scanner(System.in);
+        System.out.print("Input letter: ");
+        String l = word.next();
+        l = "A, a, B, b, C, c, D, d, E, e, F, f, G, g, H, h, I, i, K, k, L, l, M, m, N, n, O, o, P, p, Q, q, R, r, S, s, T, t, V, v, X, x, Y, y, Z, z";
+
+         }
+
+        }
